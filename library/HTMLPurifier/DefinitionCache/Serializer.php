41d521b6e35c5c9859064fc3aa98b5fbf7cd4229
obernado <obernado@firmaprofesional.com>
2023-10-10 12:44:24 +0200

    remove problematic permissions

--- library/HTMLPurifier/DefinitionCache/Serializer.php
+++ library/HTMLPurifier/DefinitionCache/Serializer.php
@@ -201,7 +201,7 @@ class HTMLPurifier_DefinitionCache_Serializer extends HTMLPurifier_DefinitionCac
             // set permissions of the new file (no execute)
             $chmod = $config->get('Cache.SerializerPermissions');
             if ($chmod !== null) {
-                chmod($file, $chmod & 0666);
+                chmod($file, $chmod);
             }
         }
         return $result;
